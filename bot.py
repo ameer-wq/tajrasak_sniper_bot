9037fb9cd03e95de09f7fa11a2e86f092050f0fe
ameer-wq <sabahameer161@gmail.com>
2025-04-15 00:03:03 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "أهلاً بيك ويا بوت تاج راسك!")
+
+bot.polling()
